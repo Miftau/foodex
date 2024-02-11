86ad11087a4aa8a5199e59180b64c63cacfa1fa2
Miftau <miftaudeenb@gmail.com>
2024-02-11 02:37:37 +0100

    ...

--- back_end/routes/auth.js
+++ back_end/routes/auth.js
@@ -4,7 +4,7 @@ const protect = require('../middleware/authmiddleware');
 
 const router = Router();
 
-router.post('/register', authController.registerUser);
+router.post('/signup', authController.registerUser);
 router.post('/login', authController.loginUser);
 router.get('/profile', protect, authController.getUserProfile);
 
